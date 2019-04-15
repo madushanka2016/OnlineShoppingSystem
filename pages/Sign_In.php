3512dac07a32b916df96bcf1e59e51ff9921d96d
madushanka2016 <madushankakahawa@gmail.com>
2019-04-15 21:19:19 +0530

    modify the fron end

--- pages/Sign_In.php
+++ pages/Sign_In.php
@@ -9,7 +9,7 @@
 	?>
 	<div class="container-fluid">
 		<div class="row">
-			<div class="col col-lg-4 col-md-5 col-sm-7 col-xs-8" autocomplete="off">
+			<div class="col col-xl-4 col-lg-5 col-md-6 col-sm-8 col-12 offset-sm-1" autocomplete="off" style="border: solid;">
 				<div class="form-group">
 					<form action="Sign_InPHP.php" method="post" >
 						<label class="" >User Name :</label>
@@ -17,14 +17,15 @@
 						<label class="">Password :</label>
 						<input class="form-control" type="password" name="password" required="">
 						<br>
-						<input class="btn btn-primary" type="submit" name="" value="Log In">
-						<br><br>
+						<div class="row justify-content-center">
+							<input class="btn btn-primary" type="submit" name="" value="Log In">
+						</div>
 					</form>
 				</div>
+				<h3>Don't have account?</h3>
+				<button class="btn btn-link"><a href="Sign_Up.php">Create Account</a></button>
 			</div>
 		</div>
-		<h3>Don't have account?</h3>
-		<button class="btn btn-link"><a href="Sign_Up.php">Create Account</a></button>
 	</div>
 </body>
 </html>
